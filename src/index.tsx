2e28216abb4735dec60abebdc802468de4b9cd2e
Aborysa <aborysa@gmail.com>
2018-07-20 23:50:50 +0200

    Fixed structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import 'assets/css/index.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
