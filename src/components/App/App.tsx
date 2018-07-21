e125b298b281117f508387ddf9ea1d82a01ffb4b
sardred <andelar96@gmail.com>
2018-07-21 15:48:24 +0200

    Tweaked folder structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as style from './App.scss';
+
+export const App = () => (
+  <div className={style.app}>
+    <header className={style.appHeader}>
+      <h1 className={style.appTitle}>Welcome to React</h1>
+    </header>
+    <p className={style.appIntro}>༼ ºل͟º༼ ºل͟º༼ ºل͟º༼ ºل͟º ༽ºل͟º ༽ºل͟º ༽</p>
+  </div>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { App } from 'src/components';
 import 'style/global.scss';
-import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
