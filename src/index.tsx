6548aa9af6dee519c1b0ea295b1e43f3082ba60e
sardred <andelar96@gmail.com>
2018-07-21 15:26:18 +0200

    Remove testing

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,5 @@ import 'assets/css/index.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
-registerServiceWorker();
