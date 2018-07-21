ca98d971ab78642265d83b8defc0cba69a14c91a
sardred <andelar96@gmail.com>
2018-07-21 15:38:07 +0200

    Added sanitize.css, removed logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import * as style from './App.scss';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
   public render() {
     return (
       <div className={style.app}>
         <header className={style.appHeader}>
-          <img src={logo} className={style.appLogo} alt="logo" />
           <h1 className={style.appTitle}>Welcome to React</h1>
         </header>
         <p className={style.appIntro}>༼ ºل͟º༼ ºل͟º༼ ºل͟º༼ ºل͟º ༽ºل͟º ༽ºل͟º ༽</p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import 'assets/css/index.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import 'style/global.scss';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
