032d51d305c5a0842263f8aa9ae801c913e4c321
Aborysa <aborysa@gmail.com>
2018-07-22 17:44:51 +0200

    Added interface for Divider props

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -3,7 +3,11 @@ import classnames from 'classnames';
 import * as React from 'react';
 import style from './Divider.scss';
 
-export function Divider(props: any) {
+export interface IDividerProps extends React.HTMLAttributes<HTMLHRElement> {
+  className?: string;
+}
+
+export function Divider(props: IDividerProps) {
   const className = classnames(props.className, style.divider);
   return <hr {...props} className={className} />;
 }
