f8f8158e5cfff18f7cbcf96c2694ba16fb8a9353
sardred <andelar96@gmail.com>
2018-07-28 20:45:58 +0200

    Added HARD_DATA and fixed alignment of tabs

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,47 +1,9 @@
 import * as React from 'react';
 import { Logo, Tabs } from 'src/components';
+import { HARD_DATA } from 'src/constants';
 import * as style from './Hero.scss';
 
 export const Hero = () => {
-  const tabs = [
-    {
-      content: [
-        'in the deepest ocean',
-        'the bottom of the sea',
-        'your eyes',
-        'they turn me',
-        'why should i stay here',
-        'why should i stay',
-      ],
-      heading: 'Organisasjonen',
-    },
-    {
-      content: [
-        'id be crazy not to follow',
-        'follow where you lead',
-        'your eyes',
-        'they turn me',
-      ],
-      heading: 'Komiteer',
-    },
-    {
-      content: [
-        'turn me on to phantoms',
-        'i follow to the edge of the earth',
-        'and fall off',
-      ],
-      heading: 'Andre sosiale verv',
-    },
-    {
-      content: [
-        'everybody leaves',
-        'if they get the chance',
-        'and this is my chance',
-      ],
-      heading: 'Annet',
-    },
-  ];
-
   return (
     <div className={style.hero}>
       <Logo />
@@ -50,7 +12,7 @@ export const Hero = () => {
         <p>[Insert other generic text]</p>
         <p>Trykk under for å lære mer om siden vår</p>
       </div>
-      <Tabs tabs={tabs} />
+      <Tabs tabs={HARD_DATA} />
     </div>
   );
 };
