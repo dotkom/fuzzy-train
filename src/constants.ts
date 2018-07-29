e2b9d3d51dcd82efc89c84ba97226adae3f26e32
Christian Duvholt <christian@duvholt.net>
2018-07-29 10:51:30 +0200

    Update dummy data with correct keys

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,16 @@
 import { ITab } from 'src/types';
 
 export const HARD_DATA = {
-  hovedstyret: {
+  om_hovedsamarbeidspartner: {
+    content: `
+    Vår hovedsamarbeidspartner er Knowit. De har vært en aktiv bidragsyter for miljøet på informatikk i mange år via bedriftspresentasjoner, kurs og arrangement i fadderukene.
+    Onlines hovedsamarbeidspartner er en sterk bidragsyter for at vi skal kunne tilby et bredt spekter av aktiviteter til våre studenter. Vi har et tett samarbeid rundt det å skape gode kurs, bedriftspresentasjoner, og ikke minst får vi mye hjelp rundt opplegget til fadderukene!
+    Vi prøver kontinuerlig å forbedre de aktivitetene vi har, samt inkludere vår hovedsamarbeidspartner i nye arrangementer.
+    `,
+    heading: 'Hovedsamarbeidspartner',
+    key: 'om_hovedsamarbeidspartner',
+  },
+  om_hovedstyret: {
     content: `
     Hovedstyret velges av linjeforeningens medlemmer på generalforsamlingen i løpet av vårsemesteret og sitter ett år frem i tid. Styret består av leder, nestleder og alle komitelederne.
     Hovedstyret er først og fremst en møteplass for koordinering av de forskjellige komiteene. Styret driver også med økonomistyring og annet administrativt arbeid.
@@ -19,16 +28,7 @@ export const HARD_DATA = {
       Spørsmål om Online? Send oss en epost (til hovedstyret@online.ntnu.no)!
       `,
     heading: 'Hovedstyret',
-    key: 'hovedstyret',
-  },
-  hsp: {
-    content: `
-    Vår hovedsamarbeidspartner er Knowit. De har vært en aktiv bidragsyter for miljøet på informatikk i mange år via bedriftspresentasjoner, kurs og arrangement i fadderukene.
-    Onlines hovedsamarbeidspartner er en sterk bidragsyter for at vi skal kunne tilby et bredt spekter av aktiviteter til våre studenter. Vi har et tett samarbeid rundt det å skape gode kurs, bedriftspresentasjoner, og ikke minst får vi mye hjelp rundt opplegget til fadderukene!
-    Vi prøver kontinuerlig å forbedre de aktivitetene vi har, samt inkludere vår hovedsamarbeidspartner i nye arrangementer.
-    `,
-    heading: 'Hovedsamarbeidspartner',
-    key: 'hsp',
+    key: 'om_hovedstyret',
   },
   om_online: {
     content: `Online er linjeforeningen for informatikkstudenter ved NTNU i Trondheim. Linjeforeningens oppgave er å forbedre studiemiljøet ved å fremme sosialt samvær, faglig kompetanse og kontakt med næringslivet.
